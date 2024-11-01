b335b28292dbdcd9250040863d89af2eb96efc36
eungikim <eungikim.me@gmail.com>
2024-11-01 17:35:41 +0900

    section2: Add update and delete methods to MemberRepositoryV0 and test
    Implemented update to modify member's money and delete to remove a member by ID. Added corresponding tests to verify the new functionalities and ensure they work as expected.

--- src/test/java/hello/jdbc/connection/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/connection/repository/MemberRepositoryV0Test.java
@@ -2,9 +2,11 @@ package hello.jdbc.connection.repository;
 
 import hello.jdbc.connection.domain.Member;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,12 +18,22 @@ class MemberRepositoryV0Test {
     @Test
     void crud() throws SQLException {
         // save
-        Member memberV0 = new Member("memberV0", 10000);
+        Member memberV0 = new Member("memberV2", 10000);
         repository.save(memberV0);
 
         // findById
         Member foundMember = repository.findById(memberV0.getMemberId());
         log.info("foundMember : {}", foundMember);
         assertThat(foundMember).isEqualTo(memberV0);
+
+        // update: money: 10000 -> 20000
+        repository.update(memberV0.getMemberId(), 20000);
+        Member updatedMember = repository.findById(memberV0.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+        // delete
+        repository.delete(memberV0.getMemberId());
+        Assertions.assertThatThrownBy(() -> repository.findById(memberV0.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
     }
 }

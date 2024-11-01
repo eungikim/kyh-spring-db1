d04a38aa4afff857b24a8c04653058488ce23548
eungikim <eungikim.me@gmail.com>
2024-11-01 17:23:15 +0900

    section2: Add findById method to MemberRepositoryV0 and test
    This commit introduces the findById method to MemberRepositoryV0, allowing retrieval of members by their ID. Additionally, a corresponding test case has been added to verify the functionality of this method within MemberRepositoryV0Test.

--- src/test/java/hello/jdbc/connection/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/connection/repository/MemberRepositoryV0Test.java
@@ -1,17 +1,27 @@
 package hello.jdbc.connection.repository;
 
 import hello.jdbc.connection.domain.Member;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
 
     @Test
     void crud() throws SQLException {
+        // save
         Member memberV0 = new Member("memberV0", 10000);
         repository.save(memberV0);
+
+        // findById
+        Member foundMember = repository.findById(memberV0.getMemberId());
+        log.info("foundMember : {}", foundMember);
+        assertThat(foundMember).isEqualTo(memberV0);
     }
 }

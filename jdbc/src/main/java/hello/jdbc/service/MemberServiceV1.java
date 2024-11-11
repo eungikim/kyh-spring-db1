3f543b1246858df5b815a482f94902230d7fab77
eungikim <eungikim.me@gmail.com>
2024-11-11 16:20:08 +0900

    section4: Add MemberServiceV1 and its unit tests
    Added `MemberServiceV1` for handling account transfers with basic logic and validation. Implemented unit tests for normal transfer and exception scenario to ensure correct functionality.

---
+++ jdbc/src/main/java/hello/jdbc/service/MemberServiceV1.java
@@ -0,0 +1,27 @@
+package hello.jdbc.service;
+
+import hello.jdbc.connection.domain.Member;
+import hello.jdbc.connection.repository.MemberRepositoryV1;
+import lombok.RequiredArgsConstructor;
+
+import java.sql.SQLException;
+
+@RequiredArgsConstructor
+public class MemberServiceV1 {
+    private final MemberRepositoryV1 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        testValidation(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private static void testValidation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}

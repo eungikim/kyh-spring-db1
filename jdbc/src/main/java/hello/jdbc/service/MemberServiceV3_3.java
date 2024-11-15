1549545d3951a73b21d987f9e194cced50f3f3d6
eungikim <eungikim.me@gmail.com>
2024-11-15 17:48:17 +0900

    section5: Add MemberServiceV3_3 with transactional AOP support
    Introduced MemberServiceV3_3 to handle member transactions with @Transactional annotation for AOP support. Also added MemberServiceV3_3Test to ensure proper functionality and transactional behavior during member transfers.

---
+++ jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_3.java
@@ -0,0 +1,42 @@
+package hello.jdbc.service;
+
+import hello.jdbc.connection.domain.Member;
+import hello.jdbc.connection.repository.MemberRepositoryV3;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.SQLException;
+
+
+/**
+ * 트랜잭션 - @Transactional AOP
+ */
+@Slf4j
+public class MemberServiceV3_3 {
+    private final MemberRepositoryV3 memberRepository;
+
+    public MemberServiceV3_3(MemberRepositoryV3 memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Transactional
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        bizLogic(toId, fromId, money);
+    }
+
+    private void bizLogic(String toId, String fromId, int money) throws SQLException {
+        // 비즈니스 로직
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        testValidation(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private static void testValidation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
